729338f708a91c9ed993cc7d3616837938a5d138
bcchaudhary <bharartchaudhary20893@gmail.com>
2019-11-27 15:58:27 +0530

    notifcation fix issue above 28

--- app/src/main/java/com/jshealth/reminderapp/AlarmReceiver.java
+++ app/src/main/java/com/jshealth/reminderapp/AlarmReceiver.java
@@ -4,6 +4,7 @@ package com.jshealth.reminderapp;
 
 
 import android.app.AlarmManager;
+import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.ComponentName;
@@ -11,9 +12,14 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.media.RingtoneManager;
+import android.os.Build;
 import android.os.SystemClock;
+import android.provider.Settings;
+import android.util.Log;
 
+import androidx.annotation.RequiresApi;
 import androidx.core.app.NotificationCompat;
 import androidx.legacy.content.WakefulBroadcastReceiver;
 
@@ -23,11 +29,13 @@ import java.util.Calendar;
 public class AlarmReceiver extends WakefulBroadcastReceiver {
     AlarmManager mAlarmManager;
     PendingIntent mPendingIntent;
-
+    private NotificationManager mNotificationManager;
+    private NotificationCompat.Builder mBuilder;
+    public static final String NOTIFICATION_CHANNEL_ID = "10001";
     @Override
     public void onReceive(Context context, Intent intent) {
         int mReceivedID = Integer.parseInt(intent.getStringExtra(ReminderEditActivity.EXTRA_REMINDER_ID));
-
+        Log.e("test","=>>>>>@@====  onReceive@@@@@ >>>>>>>>>");
         // Get notification title from Reminder Database
         ReminderDatabase rb = new ReminderDatabase(context);
         Reminder reminder = rb.getReminder(mReceivedID);
@@ -38,20 +46,63 @@ public class AlarmReceiver extends WakefulBroadcastReceiver {
         editIntent.putExtra(ReminderEditActivity.EXTRA_REMINDER_ID, Integer.toString(mReceivedID));
         PendingIntent mClick = PendingIntent.getActivity(context, mReceivedID, editIntent, PendingIntent.FLAG_UPDATE_CURRENT);
 
-        // Create Notification
-        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(context)
-                .setLargeIcon(BitmapFactory.decodeResource(context.getResources(), R.mipmap.ic_launcher))
-                .setSmallIcon(R.drawable.ic_alarm_on_white_24dp)
-                .setContentTitle(context.getResources().getString(R.string.app_name))
-                .setTicker(mTitle)
+        mBuilder = new NotificationCompat.Builder(context);
+        mBuilder.setSmallIcon(R.mipmap.ic_launcher);
+        mBuilder.setContentTitle(context.getResources().getString(R.string.app_name))
                 .setContentText(mTitle)
-                .setSound(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION))
-                .setContentIntent(mClick)
-                .setAutoCancel(true)
-                .setOnlyAlertOnce(true);
+                .setAutoCancel(false)
+                .setSound(Settings.System.DEFAULT_NOTIFICATION_URI)
+                .setContentIntent(mClick);
 
+        mNotificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O)
+        {
+            int importance = NotificationManager.IMPORTANCE_HIGH;
+            NotificationChannel notificationChannel = new NotificationChannel(NOTIFICATION_CHANNEL_ID, "NOTIFICATION_CHANNEL_NAME", importance);
+            notificationChannel.enableLights(true);
+            notificationChannel.setLightColor(Color.RED);
+            notificationChannel.enableVibration(true);
+            notificationChannel.setVibrationPattern(new long[]{100, 200, 300, 400, 500, 400, 300, 200, 400});
+            assert mNotificationManager != null;
+            mBuilder.setChannelId(NOTIFICATION_CHANNEL_ID);
+            mNotificationManager.createNotificationChannel(notificationChannel);
+        }
+        assert mNotificationManager != null;
+        mNotificationManager.notify(0 /* Request Code */, mBuilder.build());
+
+/*
         NotificationManager nManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-        nManager.notify(mReceivedID, mBuilder.build());
+        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(context);
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            String channelId = "some_channel_id";
+            CharSequence channelName = "Some Channel";
+            int importance = NotificationManager.IMPORTANCE_LOW;
+            NotificationChannel notificationChannel = null;
+            notificationChannel = new NotificationChannel(channelId, channelName, importance);
+            notificationChannel.enableLights(true);
+            notificationChannel.setLightColor(Color.RED);
+            notificationChannel.enableVibration(true);
+            notificationChannel.setVibrationPattern(new long[]{100, 200, 300, 400, 500, 400, 300, 200, 400});
+            nManager.createNotificationChannel(notificationChannel);
+        }else {
+            mBuilder.setLargeIcon(BitmapFactory.decodeResource(context.getResources(), R.mipmap.ic_launcher))
+                    .setSmallIcon(R.drawable.ic_alarm_on_white_24dp)
+                    .setContentTitle(context.getResources().getString(R.string.app_name))
+                    .setTicker(mTitle)
+                    .setContentText(mTitle)
+                    .setSound(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION))
+                    .setContentIntent(mClick)
+                    .setAutoCancel(true)
+                    .setOnlyAlertOnce(true);
+
+
+
+        }
+
+        nManager.notify(mReceivedID, mBuilder.build());*/
+        // Create Notification
+
     }
 
     public void setAlarm(Context context, Calendar calendar, int ID) {
